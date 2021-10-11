c78e376803151888f25ecece69231ef685d0ef76
kakaacat <1912445179@qq.com>
2021-10-11 17:29:06 +0800

    springMVC

---
+++ SpringMVC/out/artifacts/springMVC_01_servlet_war_exploded/WEB-INF/jsp/test.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 19124
+  Date: 2021/10/11
+  Time: 17:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+
+${msg}
+
+
+</body>
+</html>
---
+++ SpringMVC/springMVC_01_servlet/src/main/java/com/kaka/servlet/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.kaka.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author : kaka
+ * @Date: 2021-10-11 17:02
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.获取前端参数
+        String method = req.getParameter("method");
+        if (method.equals("add")){
+            req.getSession().setAttribute("msg","调用了add方法");
+        } else if (method.equals("delete")){
+            req.getSession().setAttribute("msg", "执行了delete方法");
+        }
+        //2.调用业务层
+        //3.视图转发或者重定向
+        req.getRequestDispatcher("/WEB-INF/jsp/test.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doGet(req, resp);
+    }
+}
