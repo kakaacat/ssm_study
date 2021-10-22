99c3e568e95e100d892ca3aad00ce91bf134b02c
kakaacat <1912445179@qq.com>
2021-10-22 15:25:02 +0800

    springMVC 拦截器、文件上传

---
+++ SpringMVC/springMVC_07_intercepter/src/main/java/com/kaka/config/MyInterceptor.java
@@ -0,0 +1,31 @@
+package com.kaka.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author : kaka
+ * @Date: 2021-10-21 10:56
+ */
+public class MyInterceptor implements HandlerInterceptor {
+
+    //return true ：执行下一个拦截器，放行
+    //return false ：不执行下一个拦截器
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("========处理前======");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("========处理后======");
+
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("========清理======");
+
+    }
+}
