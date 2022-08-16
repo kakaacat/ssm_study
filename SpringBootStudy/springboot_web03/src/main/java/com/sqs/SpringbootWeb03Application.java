408561d8c5f9595530c3c86ba02754f5421b735e
kakaacat <1912445179@qq.com>
2022-08-16 12:17:14 +0800

    springboot-5

---
+++ SpringBootStudy/springboot_web03/src/main/java/com/sqs/SpringbootWeb03Application.java
@@ -0,0 +1,13 @@
+package com.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeb03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWeb03Application.class, args);
+    }
+
+}
---
+++ SpringBootStudy/springboot_web03/src/test/java/com/sqs/SpringbootWeb03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeb03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
