e19e61cb104c1ab65846f8e2fba731360ba855e5
kakaacat <1912445179@qq.com>
2022-08-22 16:08:36 +0800

    springboot-8

--- SpringBootStudy/springboot_web05/src/main/java/com/sqs/config/MyMvcConfig.java
+++ SpringBootStudy/springboot_web05/src/main/java/com/sqs/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.sqs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,4 +24,16 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public LocaleResolver localeResolver() {
         return new MyLocaleResolver();
     }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html",
+                        "/",
+                        "/usr/login",
+                        "/css/**", "/js/**", "/img/**");
+    }
+
 }
--- SpringBootStudy/springboot_web05/src/main/java/com/sqs/controller/LoginController.java
+++ SpringBootStudy/springboot_web05/src/main/java/com/sqs/controller/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * @Author : kaka
  * @Date: 2022-08-18 15:28
@@ -17,8 +19,9 @@ public class LoginController {
     @RequestMapping("/user/login")
     public String login(@RequestParam("username") String userName,
                         @RequestParam("password") String password,
-                        Model model) {
+                        Model model, HttpSession session) {
         if (!StringUtils.isEmpty(userName) && "123".equals(password)) {
+            session.setAttribute("loginUser", userName);
             return "redirect:/main.html";
         } else {
             model.addAttribute("msg", "用户名或密码错误！");
