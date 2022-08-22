d1207164c424c74bb19aad3befc1d228eed86f29
kakaacat <1912445179@qq.com>
2022-08-22 16:14:48 +0800

    springboot-8

---
+++ SpringBootStudy/springboot_web05/src/main/java/com/sqs/config/LoginHandlerInterceptor.java
@@ -0,0 +1,24 @@
+package com.sqs.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-08-22 15:50
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (loginUser == null) {
+            request.setAttribute("msg", "没有权限，请先登录！");
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
